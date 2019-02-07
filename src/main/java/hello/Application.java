26bf57a99fc2ea557689645836688ea4947dc3d6
kernelold <kernel.old@gmail.com>
2019-02-07 17:41:31 +0300

    Create Application.java

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,31 @@
+package app-time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+import java.util.TimeZone;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    Date date = new Date();
+    String strDateFormat = "hh:mm:ss a";
+    DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+    String formattedDate = dateFormat.format(date);
+
+    @RequestMapping("/")
+    public String hello() {
+        return formattedDate;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
